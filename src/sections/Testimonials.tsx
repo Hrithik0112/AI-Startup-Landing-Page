5ed04b85b40d824eafc1b7a44b40e4de4b6efa87
Hrithik <slumdog4321@gmail.com>
2024-08-08 23:48:33 +0530

    Feat : added animation in testimonials section

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -1,9 +1,11 @@
+"use client"
 import React from "react";
 import avatar1 from "@/assets/avatar-1.png";
 import avatar2 from "@/assets/avatar-2.png";
 import avatar3 from "@/assets/avatar-3.png";
 import avatar4 from "@/assets/avatar-4.png";
 import Image from "next/image";
+import {motion} from "framer-motion"
 
 const testimonials = [
   {
@@ -40,10 +42,20 @@ const Testimonials = () => {
         <p className="text-lg md:text-xl text-center tracking-tight mt-5 text-white/70 max-w-sm mx-auto">
           Our revolutionary AI SEO tools have transformed our client strategies
         </p>
-        <div className="overflow-hidden mt-10 [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
+        <div className="flex overflow-hidden mt-10 [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
 
-        <div className="flex gap-5">
-          {testimonials.map((testimonial) => (
+        <motion.div 
+        initial={{
+          translateX :"-50%"
+        }}
+        animate={{translateX : "0"}}
+        transition={{
+          repeat: Infinity,
+          ease: "linear",
+          duration : 30,
+        }}
+        className="flex flex-none pr-5 gap-5">
+          {[...testimonials, ...testimonials].map((testimonial) => (
             <div
               key={testimonial.name}
               className="border max-w-xs md:max-w-md border-white/15 p-6 md:p-10 bg-[linear-gradient(to_bottom_left,rgb(140,69,255,0.3),black)] rounded-xl flex-none"
@@ -64,7 +76,7 @@ const Testimonials = () => {
               </div>
             </div>
           ))}
-        </div>
+        </motion.div>
         </div>
       </div>
     </section>
